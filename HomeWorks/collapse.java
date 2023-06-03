a57619ac28ad8fe71d7b3178da526642c6b03476
Yulya Lobanova <yuliyazinina@gmail.com>
2023-06-03 17:45:46 +0800

    Added file collapse.java

---
+++ HomeWorks/collapse.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class collapse {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1, 1, 2, 2, 2, 2, 3, 4, 4, 5, 5, 6};
+        collapseMethod(arr);
+    }
+
+    public static void collapseMethod(int[] array) {
+        
+        System.out.println(Arrays.toString(array));
+    }
+}
